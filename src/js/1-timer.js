4cfaf3626c17085fbd3b6615398a8025a814fc99
Olexandr Brylov <kwinto7@gmail.com>
2025-07-17 11:10:38 +0300

    add html and js files with hw task1 and task2

---
+++ src/js/1-timer.js
@@ -0,0 +1,133 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const startBtn = document.querySelector("[data-start]");
+const dateInput = document.querySelector("#datetime-picker");
+const daysSpan = document.querySelector("[data-days]");
+const hoursSpan = document.querySelector("[data-hours]")
+const minutesSpan = document.querySelector("[data-minutes]")
+const secondsSpan = document.querySelector("[data-seconds]")
+const timerContainer = document.querySelector(".timer")
+const parentDaysSpan = document.querySelector("[data-days]").parentElement;
+const parentHoursSpan = document.querySelector("[data-hours]").parentElement;
+const parentMinuteSpan = document.querySelector("[data-minutes]").parentElement;
+const parentSecondSpan = document.querySelector("[data-seconds]").parentElement;
+
+let userSelectedDate = null;
+let timerId = null;
+
+startBtn.disabled = true;
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        const selectedDate = selectedDates[0]
+        const now = new Date()   
+        if (selectedDate <= now) {
+            iziToast.warning({
+                iconUrl: 'img/close-circle-svgrepo-com.svg',
+                iconColor: 'black',
+                message: 'Please choose the date in the future',
+                messageColor: 'white',
+                color: '#e02828ff',
+                position: 'topRight',
+                close: false,
+            });
+            startBtn.disabled = true;
+            userSelectedDate = null;
+            return;
+        };
+        userSelectedDate = selectedDate;
+        startBtn.disabled = false;
+    },
+};
+
+flatpickr(dateInput, options);
+
+startBtn.addEventListener("click", btnClickHandler);
+
+function btnClickHandler() { 
+    if (!userSelectedDate) { 
+        return;
+    };
+    startBtn.disabled = true;
+    dateInput.disabled = true;
+
+    timerId = setInterval(() => { 
+        const now = new Date();
+        const delta = userSelectedDate - now;
+        if (delta <= 0) { 
+            clearInterval(timerId)
+            updateTimerDisplay(0);
+            return;
+        }
+        updateTimerDisplay(delta);
+    }, 1000);
+};
+
+function updateTimerDisplay(ms) {
+    const { days, hours, minutes, seconds } = convertMs(ms);
+
+    daysSpan.textContent = addLeadingZero(days);
+    hoursSpan.textContent = addLeadingZero(hours);
+    minutesSpan.textContent = addLeadingZero(minutes);
+    secondsSpan.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+    const second = 1000;
+    const minute = 60 * second;
+    const hour = 60 * minute;
+    const day = 24 * hour;
+
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor((ms % hour) / minute);
+    const seconds = Math.floor((ms % minute) / second);
+
+    return { days, hours, minutes, seconds };
+}
+
+Object.assign(dateInput.style, {
+    marginTop: `${20}px`,
+    height: `${25}px`,
+});
+
+Object.assign(startBtn.style, {
+    height: `${25}px`,
+});
+
+Object.assign(timerContainer.style, {
+    display: 'flex',
+    marginTop: `${20}px`,
+    gap: `${10}px`
+});
+  
+[parentDaysSpan, parentHoursSpan, parentMinuteSpan, parentSecondSpan].forEach(el =>  
+    Object.assign(el.style, {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontFamily: 'Inter, sans-serif',
+        textTransform: 'uppercase',
+        fontSize:`${10}px`
+  })
+);
+
+[daysSpan, hoursSpan, minutesSpan, secondsSpan].forEach((el) => { 
+    Object.assign(el.style, {
+        fontSize: `${30}px`,
+    });
+});
+
+
+
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,67 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const label = document.querySelector('.form label')
+const input = label.querySelector('input')
+const fieldset = form.querySelector('fieldset')
+const button = form.querySelector('button')
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+const delay = Number(form.elements.delay.value);
+const state = form.elements.state.value;
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (state === 'fulfilled') {
+            resolve(delay);
+        } else {
+            reject(delay);
+        }
+    }, delay);
+});
+
+promise
+    .then(delay => {
+        iziToast.success({
+            title: '✅',
+            message: `Fulfilled promise in ${delay}ms`,
+            position: 'topRight',
+            messageColor: 'white',
+            color: '#38ca27ff',
+            close: false,
+        });
+    })
+    .catch(delay => {
+        iziToast.error({
+            title: '❌',
+            message: `Rejected promise in ${delay}ms`,
+            position: 'topRight',
+            messageColor: 'white',
+            color: '#d65a25ff',
+            close: false,
+        });
+    });
+});
+
+Object.assign(form.style, {
+    marginTop: `${24}px`,
+    fontFamily: 'Montserrat, sans-serif',
+    width: 250,
+});
+
+Object.assign(input.style, {
+    display: 'block',
+    marginTop: `${4}px`,
+    width: 250,
+    height: `${32}px`,
+    marginBottom: `${8}px`,
+});
+
+Object.assign(button.style, {
+    marginTop: `${8}px`,
+    width: 250,
+    height:`${24}px`,
+});
